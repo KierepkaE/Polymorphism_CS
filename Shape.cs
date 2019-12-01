af88fff45cc1407de9ad412db2572d2a5184f049
kierepkae <kierepkaedyta@gmail.com>
2019-12-01 12:05:41 +0000

    refactor

--- Shape.cs
+++ Shape.cs
@@ -5,7 +5,6 @@ namespace polymorphism {
   public class Shape {
     public int Height { get; set; }
     public int Width { get; set; }
-    // public Position Position { get; set; }
 
     public virtual void Draw () {
       Console.WriteLine ("Parent draw . . . ");
